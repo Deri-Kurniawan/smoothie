641438cd5a4244d409ec0aed5a9b02d3f1867825
Deri Kurniawan <deri.netuchi@gmail.com>
2024-06-08 15:40:17 +0700

    feat: implement react router, page, & provider - assign smoothies data to smoothie provider - css variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from "react-router";
+import GuestLayout from "./components/layouts/guest-layout";
+import { Smoothie } from "./hooks/use-smoothie";
+import Home from "./pages/Home";
+import { SmoothieProvider } from "./providers/smoothie-provider";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const smoothies: Smoothie[] = [
+    {
+      name: "Oatmeal Cranberry Smoothie",
+      description: "A delicious and healthy oatmeal cranberry smoothie.",
+      image: "/images/smoothies/bowl1.webp",
+      imageFruitBase: "/images/smoothies/bowl1-fruitbase.webp",
+      benefits: ["Antioxidants", "Vitamins", "Minerals", "Nutrients"],
+      color: "text-primary",
+      bgColor: "bg-primary",
+      fillColor: "fill-primary",
+    },
+    {
+      name: "Blueberry Banana Smoothie",
+      description: "A refreshing blueberry banana smoothie.",
+      image: "/images/smoothies/bowl2.webp",
+      imageFruitBase: "",
+      benefits: [
+        "Rich in Antioxidants",
+        "Good Source of Fiber",
+        "Boosts Immunity",
+        "Provides Energy",
+      ],
+      color: "text-secondary-blue",
+      bgColor: "bg-secondary-blue",
+      fillColor: "fill-secondary-blue",
+    },
+    {
+      name: "Cucumber Smoothie",
+      description: "A refreshing cucumber smoothie.",
+      image: "/images/smoothies/bowl3.webp",
+      imageFruitBase: "",
+      benefits: [
+        "Hydration",
+        "Low in Calories",
+        "Rich in Nutrients",
+        "Promotes Detoxification",
+      ],
+      color: "text-secondary-green",
+      bgColor: "bg-secondary-green",
+      fillColor: "fill-secondary-green",
+    },
+    {
+      name: "Mango-Almond Smoothie",
+      description: "A delicious mango-almond smoothie.",
+      image: "/images/smoothies/bowl4.webp",
+      imageFruitBase: "",
+      benefits: [
+        "Rich in vitamin C",
+        "Healthy fats",
+        "Provides Energy",
+        "Supports Skin Health",
+      ],
+      color: "text-secondary-orange",
+      bgColor: "bg-secondary-orange",
+      fillColor: "fill-secondary-orange",
+    },
+  ].map((smoothie, index) => ({ ...smoothie, id: ++index }));
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <GuestLayout>
+      <SmoothieProvider smoothies={smoothies}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/specialties" element={<Home />} />
+          <Route path="/about" element={<Home />} />
+          <Route path="/contact-us" element={<Home />} />
+        </Routes>
+      </SmoothieProvider>
+    </GuestLayout>
+  );
 }
 
-export default App
+export default App;
